49bf8f0504e3d6c0bed927ff05bf70b9500ac931
ccouturas <coline.couturas@yogoko.fr>
2020-06-16 10:12:40 +0200

    Adding CCommande files.

--- ROBIOT_SIMU/CCommande.cpp
+++ ROBIOT_SIMU/CCommande.cpp
@@ -1,1 +1,29 @@
 #include "CCommande.h"
+
+
+coordonnees getCoordonnees(int iNumero){
+
+	if(iNumero < tab_coordonnees.size) {
+		return tab_coordonnees[iNumero];
+	}
+}
+
+void lireTableau(){
+	
+	FILE *pFile;
+	pFile = fopen("LISTE DE CONTROLE", "r");
+	if (pFile == NULL) {
+	cout << "Probleme ouverture du fichier." << endl;
+	}
+	else {
+		do{
+			for(int i = 0, i < tab_coordonnees.size, i++){
+				pFile >> tab_coordonnees[i].x;
+				pFile >> tab_coordonnees[i].y;
+			}
+		}while(pFile != EOF);
+
+		fclose(pFile);
+	}
+
+}
